007a483f71f95257a2dac95464c87c0c56390f6f
acv5255 <acv5255@psu.edu>
2023-08-07 22:07:41 -0400

    Adding framework for new input file handling

---
+++ include/input_files.hpp
@@ -0,0 +1,38 @@
+/*
+    Input files for BioRT-HBV
+*/
+using f64 = double;
+
+class CiniFile {
+
+};
+
+class ChemFile {
+
+};
+
+class SoilFile {
+    public:
+        struct SoilParameters {
+            f64 porosity;       // Porosity [0,1]
+            f64 ws_passive;     // Passive water storage [mm*m^2/m^2]
+            f64 depth;          // Depth of zone [mm]
+        };
+
+};
+
+class PrecipChem {
+
+};
+
+class HBVResults {
+
+};
+
+class HBVParameters {
+
+};
+
+class Database {
+
+};
